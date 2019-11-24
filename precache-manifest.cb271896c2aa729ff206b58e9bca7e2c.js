0bc344bad4b78532f32ce39b1840bba382eb26c4
yesh86 <yeshwant86@gmail.com>
2019-11-24 00:17:52 -0500

    Updates

---
+++ precache-manifest.cb271896c2aa729ff206b58e9bca7e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c254c60697dcb5a489344d1e42f4dbd4",
+    "url": "/reactonaws/index.html"
+  },
+  {
+    "revision": "be583868d9735089cea6",
+    "url": "/reactonaws/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4e0c209d74f03ff2376a",
+    "url": "/reactonaws/static/js/2.63af83f1.chunk.js"
+  },
+  {
+    "revision": "be583868d9735089cea6",
+    "url": "/reactonaws/static/js/main.c327e1a4.chunk.js"
+  },
+  {
+    "revision": "0458428bedc9677dcf1f",
+    "url": "/reactonaws/static/js/runtime-main.18e98fda.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactonaws/static/media/logo.25bf045c.svg"
+  }
+]);
